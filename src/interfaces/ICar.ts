a1349fbc752159c19591ecc46c04d75d4ad3adc8
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-16 18:02:05 -0300

    Criada a interface ICar. Enviando requisito 3

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+export const carZodSchema = vehicleZodSchema.merge(z.object({
+  doorsQty: z.number({
+    invalid_type_error: 'doorsQty must be a number',
+  }).int().gte(2, {
+    message: 'doorsQty must be greater than or equal to 2',
+  }).lte(4, {
+    message: 'doorsQty cannot be greater than 4',
+  }),
+  seatsQty: z.number({
+    invalid_type_error: 'seatsQty must be a number',
+  }).int().gte(2, {
+    message: 'seatsQty must be greater than or equal to 2',
+  }).lte(7, {
+    message: 'seatsQty cannot be greater than 7',
+  }),
+}));
+
+export type Icar = z.infer<typeof carZodSchema>;

24db3f72a058985d81336edb1c6661b97510d9c7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-17 00:46:48 -0300

    Criado o Model de carros

---
+++ src/models/Car.Model.ts
@@ -0,0 +1,21 @@
+import { Schema, model as MongoModel } from 'mongoose';
+import Model from './Mongo.Model';
+import { ICar } from '../interfaces/ICar';
+
+const carSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarModel extends Model<ICar> {
+  constructor(model = MongoModel<ICar>('Car', carSchema)) {
+    super(model);
+  }
+}
+
+export default CarModel;

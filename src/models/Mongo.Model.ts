ba19ae3647ad22c24ad13fce3fd78c71ad75f845
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-17 00:46:19 -0300

    Criado o Model genérico

---
+++ src/models/Mongo.Model.ts
@@ -0,0 +1,56 @@
+import { Model as MongoModel, isValidObjectId, UpdateQuery } from 'mongoose';
+import CodeError from '../errors/CodeErrors';
+import { IModel } from '../interfaces/IModel';
+
+abstract class GenericModel<T> implements IModel<T> {
+  private _model: MongoModel<T>;
+  private _message = 'Invalid MongoId';
+
+  constructor(model: MongoModel<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create(obj);
+  }
+
+  public async read(): Promise<T[]> {
+    const vehicles = await this._model.find();
+
+    return vehicles;
+  }
+
+  public async readOne(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new CodeError(this._message, 400);
+    }
+
+    return this._model.findOne({ _id: id });
+  }
+
+  public async update(id: string, obj: Partial<T>): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new CodeError(this._message, 400);
+    }
+
+    const updatedVehicle = await this._model.findByIdAndUpdate(
+      { _id: id }, 
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );
+
+    return updatedVehicle as T;
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) {
+      throw new CodeError(this._message, 400);
+    }
+
+    const result = await this._model.deleteOne({ _id: id });
+
+    return result as unknown as T;
+  }
+}
+
+export default GenericModel;

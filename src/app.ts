cbd2a2c24b6c658f94136bebff9ad7b38b29a005
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-18 11:44:38 -0300

    Alterado o nome do Arquivo de rotas de carros

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import errors from './middlewares/Error.Middleware';
-import carsRoute from './routes/Car.routes';
+import carsRoute from './routes/Car.Routes';
 
 const app = express();
 app.use(express.json());

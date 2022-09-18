fc4b4db2cbdfa77d7cfeabcd5be16272b4bf27d1
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-18 12:05:58 -0300

    Adicionado ao app.ts o endpoint "/motorcycles"

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errors from './middlewares/Error.Middleware';
 import carsRoute from './routes/Car.Routes';
+import motoRoute from './routes/Motorcycle.Routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carsRoute);
+app.use('/motorcycles', motoRoute);
 
 app.use(errors);
 
